6d536ae272ff731617e0797c4d5e9f66c9984d1b
Azekeeee <pro.skydoodle@gmail.com>
2023-10-12 00:05:58 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println("Привет, мир!");
+        System.out.println("Я учу java.");
+        System.out.println("100");
+        System.out.println("100+200");
+        System.out.println("5+6=" + (5 + 6));
+
+         */
+        System.out.print("Привет, мир! ");
+        System.out.println("Привет, Java!");
+
+        //компоненты 123
+        System.out.println("Компоненты Java:");
+        System.out.print("JVM, JRE, JDK");
+    }
+}
